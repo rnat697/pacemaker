633c4d956b4851bc86fa10baacdd54caeb00ed61
rnat697 <79692362+rnat697@users.noreply.github.com>
2022-08-23 11:52:21 +1200

    added eclipse file for lab2 but has undefined reference for reset and tick

---
+++ lab2/DE2-115/DE2-115/incremental_db/compiled_partitions/nios2_system.root_partition.map.hbdb.sig
@@ -0,0 +1,1 @@
+ec11a242691b72eb8ec5b0acbfbcf5b3
---
+++ lab2/cs303__de2_lab2/lab2_and_scchart/obj/default/hello_world.d
@@ -0,0 +1,19 @@
+obj/default/hello_world.o: hello_world.c \
+ //files.auckland.ac.nz/myhome/Documents/GitHub/ddd-mode-pacemaker-303-group-10/lab2/cs303__de2_lab2/lab2_and_scchart_bsp/system.h \
+ //files.auckland.ac.nz/myhome/Documents/GitHub/ddd-mode-pacemaker-303-group-10/lab2/cs303__de2_lab2/lab2_and_scchart_bsp/linker.h \
+ \\files.auckland.ac.nz\myhome\documents\github\ddd-mode-pacemaker-303-group-10\lab2\cs303__de2_lab2\lab2_and_scchart_bsp\drivers\inc\altera_avalon_pio_regs.h \
+ \\files.auckland.ac.nz\myhome\documents\github\ddd-mode-pacemaker-303-group-10\lab2\cs303__de2_lab2\lab2_and_scchart_bsp\hal\inc\io.h \
+ \\files.auckland.ac.nz\myhome\documents\github\ddd-mode-pacemaker-303-group-10\lab2\cs303__de2_lab2\lab2_and_scchart_bsp\hal\inc\alt_types.h \
+ //files.auckland.ac.nz/myhome/Documents/GitHub/ddd-mode-pacemaker-303-group-10/lab2/cs303__de2_lab2/lab2_and_scchart_bsp/sccharts.h
+
+//files.auckland.ac.nz/myhome/Documents/GitHub/ddd-mode-pacemaker-303-group-10/lab2/cs303__de2_lab2/lab2_and_scchart_bsp/system.h:
+
+//files.auckland.ac.nz/myhome/Documents/GitHub/ddd-mode-pacemaker-303-group-10/lab2/cs303__de2_lab2/lab2_and_scchart_bsp/linker.h:
+
+\\files.auckland.ac.nz\myhome\documents\github\ddd-mode-pacemaker-303-group-10\lab2\cs303__de2_lab2\lab2_and_scchart_bsp\drivers\inc\altera_avalon_pio_regs.h:
+
+\\files.auckland.ac.nz\myhome\documents\github\ddd-mode-pacemaker-303-group-10\lab2\cs303__de2_lab2\lab2_and_scchart_bsp\hal\inc\io.h:
+
+\\files.auckland.ac.nz\myhome\documents\github\ddd-mode-pacemaker-303-group-10\lab2\cs303__de2_lab2\lab2_and_scchart_bsp\hal\inc\alt_types.h:
+
+//files.auckland.ac.nz/myhome/Documents/GitHub/ddd-mode-pacemaker-303-group-10/lab2/cs303__de2_lab2/lab2_and_scchart_bsp/sccharts.h:
