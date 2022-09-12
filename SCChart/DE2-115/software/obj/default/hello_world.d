f08728847e198a757b6675f48a46d9e29f09d880
rnat697 <79692362+rnat697@users.noreply.github.com>
2022-09-12 14:24:59 +1200

    AS and Vs LED working

---
+++ SCChart/DE2-115/software/obj/default/hello_world.d
@@ -0,0 +1,36 @@
+obj/default/hello_world.o: hello_world.c ../HeLlOwOrLD_bsp/system.h \
+ ../HeLlOwOrLD_bsp/linker.h \
+ ../HeLlOwOrLD_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../HeLlOwOrLD_bsp//HAL/inc/io.h ../HeLlOwOrLD_bsp//HAL/inc/alt_types.h \
+ ../HeLlOwOrLD_bsp//HAL/inc/sys/alt_alarm.h \
+ ../HeLlOwOrLD_bsp//HAL/inc/sys/alt_llist.h \
+ ../HeLlOwOrLD_bsp//HAL/inc/alt_types.h \
+ ../HeLlOwOrLD_bsp//HAL/inc/priv/alt_alarm.h \
+ ../HeLlOwOrLD_bsp//HAL/inc/sys/alt_irq.h \
+ ../HeLlOwOrLD_bsp//HAL/inc/nios2.h pacecharts.h timing.h
+
+../HeLlOwOrLD_bsp/system.h:
+
+../HeLlOwOrLD_bsp/linker.h:
+
+../HeLlOwOrLD_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../HeLlOwOrLD_bsp//HAL/inc/io.h:
+
+../HeLlOwOrLD_bsp//HAL/inc/alt_types.h:
+
+../HeLlOwOrLD_bsp//HAL/inc/sys/alt_alarm.h:
+
+../HeLlOwOrLD_bsp//HAL/inc/sys/alt_llist.h:
+
+../HeLlOwOrLD_bsp//HAL/inc/alt_types.h:
+
+../HeLlOwOrLD_bsp//HAL/inc/priv/alt_alarm.h:
+
+../HeLlOwOrLD_bsp//HAL/inc/sys/alt_irq.h:
+
+../HeLlOwOrLD_bsp//HAL/inc/nios2.h:
+
+pacecharts.h:
+
+timing.h:
